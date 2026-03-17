a1f04c486b0e6ca0302807076aeac8a0f9a9cb5c
rob587 <robertocamm02@yahoo.com>
2026-03-17 20:51:51 +0100

    Implement basic input state management and UI for movie search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import movies from "./movies.json";
 
 function App() {
-  return <></>;
+  //creazione stati
+
+  const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  return (
+    <>
+      <h1>Movie Search App</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <button>Cerca</button>
+    </>
+  );
 }
 
 export default App;
