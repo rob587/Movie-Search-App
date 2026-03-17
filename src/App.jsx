e30cbd4e818f513a37a03129534b66d3d464fb65
rob587 <robertocamm02@yahoo.com>
2026-03-17 21:22:14 +0100

    Fix initial state for filtered movies and update rendering logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   //creazione stati
 
   const [inputValue, setInputValue] = useState("");
-  const [filteredMovie, setFilteredMovie] = useState([]);
+  const [filteredMovie, setFilteredMovie] = useState(movies);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -24,17 +24,19 @@ function App() {
     <>
       <h1>Movie Search App</h1>
       <input
+        onKeyDown={(e) => e.key === "Enter" && searchMovies()}
         type="text"
         value={inputValue}
         onChange={(e) => {
           setInputValue(e.target.value);
+          if (e.target.value === "") setFilteredMovie(movies);
         }}
       />
       <button onClick={searchMovies}>Cerca</button>
 
-      {movies.map((movie) => {
+      {filteredMovie.map((movie) => {
         return (
-          <Card style={{ width: "18rem" }}>
+          <Card key={movie.id} style={{ width: "18rem" }}>
             <Card.Img src={movie.poster} />
             <Card.Body>
               <Card.Title>{movie.title}</Card.Title>
