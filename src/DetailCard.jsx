bf4ad3982a8515a1839d1cabd67aa7796ea1a818
rob587 <robertocamm02@yahoo.com>
2026-03-18 22:22:09 +0100

    Implement DetailCard component to display movie details using Bootstrap

--- src/DetailCard.jsx
+++ src/DetailCard.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import movies from "./movies.json";
+import Card from "react-bootstrap/Card";
+import { useParams } from "react-router-dom";
 
 const DetailCard = () => {
-  return <div></div>;
+  const { id } = useParams();
+
+  const movie = movies.find((movie) => movie.id === Number(id));
+  return (
+    <>
+      <Card>
+        <Card.Img variant="top" src={movie.poster} />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.description}</Card.Text>
+          <Card.Text>{movie.genre}</Card.Text>
+          <Card.Text>{movie.duration}</Card.Text>
+          <Card.Text>{movie.rating}</Card.Text>
+          <Card.Text>{movie.year}</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
 };
 
 export default DetailCard;
